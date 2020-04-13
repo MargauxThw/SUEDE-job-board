d84fb01b55802ce0cee94012850667a2492d6925
margaux <thwaitesmargaux@gmail.com>
2020-04-13 17:57:16 +1000

    Updates

---
+++ precache-manifest.981b7acbf067988f863f96d6dc65d0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39c33f085a1bb6127af3eeae2020c11",
+    "url": "/SUEDE-job-board/index.html"
+  },
+  {
+    "revision": "bfacf751fef483b91dec",
+    "url": "/SUEDE-job-board/static/css/main.3ad1a8ac.chunk.css"
+  },
+  {
+    "revision": "ac2e179f93a7598f98c9",
+    "url": "/SUEDE-job-board/static/js/2.c493cfef.chunk.js"
+  },
+  {
+    "revision": "91dbce2febfc227edf628f0fd0b15354",
+    "url": "/SUEDE-job-board/static/js/2.c493cfef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfacf751fef483b91dec",
+    "url": "/SUEDE-job-board/static/js/main.4d571426.chunk.js"
+  },
+  {
+    "revision": "6425c35842ed75b991b6",
+    "url": "/SUEDE-job-board/static/js/runtime-main.581dac7b.js"
+  }
+]);
